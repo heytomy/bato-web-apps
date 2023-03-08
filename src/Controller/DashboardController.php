470b218b9df3faaf0bb53a96de45bbe583f7a01c
Thomas NISI <nisi.thomas@outlook.fr>
2023-03-08 11:13:31 +0100

    secitury login

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -6,14 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class DashboardController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/dashboard', name: 'app_dashboard')]
     public function index(): Response
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('dashboard/index.html.twig', [
+            'controller_name' => 'DashboardController',
         ]);
     }
 }

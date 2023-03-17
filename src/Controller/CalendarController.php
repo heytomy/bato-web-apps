5bb4b00a840d08feeb720ad2e922d623ac270292
Thomas NISI <nisi.thomas@outlook.fr>
2023-03-17 10:37:41 +0100

    delete package

--- src/Controller/CalendarController.php
+++ src/Controller/CalendarController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CalendarController extends AbstractController
@@ -16,25 +15,4 @@ class CalendarController extends AbstractController
             'controller_name' => 'CalendarController',
         ]);
     }
-
-    #[Route('/events', name: 'app_calendar_events')]
-    public function getCalendarEvents(): JsonResponse
-    {
-        // Get events from your database or another source
-        $events = [
-            [
-                'title' => 'Event 1',
-                'start' => '2023-03-20T09:00:00',
-                'end' => '2023-03-20T10:00:00',
-            ],
-            [
-                'title' => 'Event 2',
-                'start' => '2023-03-21T14:00:00',
-                'end' => '2023-03-21T15:00:00',
-            ],
-        ];
-
-        return new JsonResponse($events);
-    }
-
 }

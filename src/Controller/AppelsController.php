550bc9f6c748300d9afec6dd7c14d8f2d8140860
Thomas NISI <nisi.thomas@outlook.fr>
2023-03-21 14:21:25 +0100

    pb namespace

--- src/Controller/AppelsController.php
+++ src/Controller/AppelsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\AppelsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class AppelsController extends AbstractController
 {
     #[Route('/appels', name: 'app_appels')]
-    public function index(): Response
+    public function index(AppelsRepository $appelsRepository): Response
     {
-        
+        $appels = $appelsRepository->findAll();
+
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
         return $this->render('appels/index.html.twig', [
-            'controller_name' => 'AppelsController',
+            'appels' => $appels,
         ]);
     }
 }

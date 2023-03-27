b2211b23b232c2bdbdc6e978c28b0bc91b8826ee
Thomas NISI <nisi.thomas@outlook.fr>
2023-03-27 16:58:34 +0200

    correction

--- src/Form/AppelsSAVType.php
+++ src/Form/AppelsSAVType.php
@@ -3,16 +3,36 @@
 namespace App\Form;
 
 use App\Entity\AppelsSAV;
+use App\Entity\ClientDef;
+use App\Repository\ClientDefRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AppelsSAVType extends AbstractType
 {
+    private $contrats;
+
+    public function __construct(ClientDefRepository $contrats)
+    {
+        $this->contrats = $contrats;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('client')
+            ->add('client', EntityType::class,[
+            'class' => ClientDef::class,
+            'choices' => $this->contrats->findByCodeClient('contrats'),
+            'label' => 'Client SAV',
+            'choice_label' => function(ClientDef $codeClient){
+                return $codeClient->getContrats();
+            },
+            'placeholder' => 'Client SAV',
+            'attr' => [
+                'class' => 'form-select'
+            ]])
             ->add('contrats')
             ->add('description')
             ->add('rdvDate')

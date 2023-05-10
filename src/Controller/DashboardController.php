a8f11400fe1093d598e2a12da92f763c469e9bd5
Thomas NISI <nisi.thomas@outlook.fr>
2023-05-10 13:40:37 +0200

    afficher le nombre de rdv d'un utilisateur

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Appels;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,35 +15,25 @@ class DashboardController extends AbstractController
     public function index(EntityManagerInterface $em): Response
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
-    
-        // Get all new appointments from the last day
-        $now = new \DateTime();
-        $oneDayAgo = (new \DateTime())->sub(new \DateInterval('P1D'));
-    
-        $newAppels = $em->getRepository(Appels::class)->createQueryBuilder('a')
-            ->where('a.createdAt BETWEEN :oneDayAgo AND :now')
-            ->setParameter('oneDayAgo', $oneDayAgo)
-            ->setParameter('now', $now)
-            ->orderBy('a.createdAt', 'DESC')
+
+        $currentUserId = $this->getUser()->getId();
+
+        $appelsCurrentUser = $em->getRepository(Appels::class)->createQueryBuilder('a')
+            ->innerJoin('a.rdv', 'rdv')
+            ->where('a.ID_Utilisateur = :userId')
+            ->setParameter('userId', $currentUserId)
+            ->andWhere('rdv.dateDebut >= :today')
+            ->andWhere('rdv.dateDebut < :tomorrow')
+            ->setParameter('today', new \DateTime('today'))
+            ->setParameter('tomorrow', new \DateTime('tomorrow'))
             ->getQuery()
-            ->getResult();
-    
-        // Get the count of new appointments from the last day
-        $countNewAppels = $em->getRepository(Appels::class)->createQueryBuilder('a')
-            ->select('COUNT(a.id)')
-            ->where('a.createdAt BETWEEN :oneDayAgo AND :now')
-            ->setParameter('oneDayAgo', $oneDayAgo)
-            ->setParameter('now', $now)
-            ->getQuery()
-            ->getSingleScalarResult();
-    
+            ->getResult(); 
+
         $em->clear();
-    
+        
         return $this->render('dashboard/index.html.twig', [
             'current_page' => 'app_dashboard',
-            'newAppels' => $newAppels,
-            'countNewAppels' => $countNewAppels,
+            'appelsCurrentUser' => $appelsCurrentUser,
         ]);
     }
-    
 }
